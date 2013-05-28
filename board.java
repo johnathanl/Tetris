ac13b4b8c579f357155e87af0a026293b2476760
Johnathan Lim <johnathan1234@gmail.com>
2013-05-29 04:33:37 +0800

    rearrange file

--- board.java
+++ board.java
@@ -6,89 +6,74 @@
  * @version (a version number or a date)
  */
 import java.util.Stack;
-import java.util.Arrays;
 
 public class board
 {
-	public static Stack<boolean[]> boardStack;
-	public static boolean[][] grid;
+	public Stack<line> boardStack;
+	public boolean[][] grid;
 	public int width;
 
 	public board (int w) {
 
 	}
 
-	public static void popBoard (int w) {
+	public void popBoard (int w) {
 		boolean[] foundHeight = new boolean[w];
-		boolean[] temp2 = new boolean[w];
 		int rowCleared =  0;
 		int count = 0;
-		boolean enough = false;
-		Stack<boolean[]> temp = (Stack)boardStack.clone();
+		Stack<line> temp = boardStack;
 
 		for (int i = 0; i < w; i++) {
 			foundHeight[i] = false;
 		}
 
-		while (!enough){
-			if (temp.empty())enough = true;
+		while (rowCleared < w){
+			if (temp.empty())rowCleared = w;
 			count++;
-			temp2 = temp.pop();
 			for (int i = 0; i < w; i++) {
-				if (temp2[i]) {
+				if (temp.pop().getLine()[i]) {
 					foundHeight[i] = true;
+					rowCleared++;
 				}
 			}
-			int count2 = 0;
-			for (int j = 0; j < foundHeight.length; j++) {
-				if (foundHeight[j])count2++;
-			}
-			if (count2 == w) enough = true;
 		}
-		System.out.print("count =");
-		System.out.println(count);
+
 		//RENEW THE GRID
 		boolean[][] grid = new boolean[w][count+4];
-		for (int i = 0; i < w; i++) {
-			for (int j = 0; j < (count+4); j++) {
+		for (int i = 0; i <= (count+4); i++) {
+			for (int j = 0; j < w; j++) {
 				grid[i][j] = false;
 			}
 		}
+
 		//POP STACK INTO GRID
-		while (!boardStack.empty()) {
-			int i = 0;
-			grid[i+3] = boardStack.pop();
-			System.out.println(Arrays.toString(grid[i+3]));
+		for (int i = 3; i <= (count+4); i++) {
+			grid[i] = boardStack.pop().getLine();
 		}
 	}
 
-	public void pushStack (int[][] input) {
-		//Stack boardStack<line> = new Stack();
-		//Check if the line is full
-		//for (int i = 0; i)
+	/**
+	 * Input a 2D boolean array and push the whole array back in the stack
+	 * @param input
+	 */
+	public void pushBoard (boolean[][] input) {
+		for (int i = 0; i < input.length; i++) {
+			for (int j = 0; j < input[0].length; j++) {
+				if (input[i][j]) System.out.print("X");
+				else System.out.print("O");
+			}
+			System.out.println();
+		}
 	}
-	public static void main(String [] args) {
-		//TESTING THE popBoard method
-		boardStack = new Stack();
-		grid = new boolean[3][4];
-		boolean[] line1 = {false,true,true,true};
-		boolean[] line2 = {false,true,false,false};
-		boolean[] line3 = {true,false,true,true};
-		boardStack.push(line1);
-		boardStack.push(line2);
-		boardStack.push(line3);
-		System.out.println("------Imginary Board-----");
-		System.out.println(Arrays.toString(line3));
-		System.out.println(Arrays.toString(line2));
-		System.out.println(Arrays.toString(line1));
-		System.out.println("------2D Array------");
-		grid[0] = line1;
-		grid[1] = line2;
-		grid[2] = line3;
-		System.out.println(Arrays.toString(grid[2]));
-		System.out.println(Arrays.toString(grid[1]));
-		System.out.println(Arrays.toString(grid[0]));
-		System.out.println("------popBoard------");
-		popBoard(4);
+	
+	public void printBoard(boolean[][] input) {
+		for (int i = 0; i < input.length; i++) {
+			for (int j = 0; j < input[0].length; j++) {
+				if (input[i][j]) System.out.print("X");
+				else System.out.print("O");
+			}
+			System.out.println();
+		}
 	}
+	//public static void main(String [] args) {}
 }
